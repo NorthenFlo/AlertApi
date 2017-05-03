466c1ebfa51af4ebe9f584fd4e21905faea7ac7d
Florent Calabrési <Northenflo@gmail.com>
2017-05-03 09:33:15 +0200

    Update AlertAPI.java

--- AlertAPI.java
+++ AlertAPI.java
@@ -31,13 +31,14 @@ public class AlertAPI {
 	
 	public static JPanel AddAlertPanel(int x_p, int y_p, int x_s, int y_s)
 	{
+		Font font;
 		alert_panel.setBounds(x_p, y_p, x_s, y_s);
 		alert_panel.setVisible(false);
 		alert_panel.setLayout(new BoxLayout(alert_panel, BoxLayout.Y_AXIS));
 		alert_panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		alert_panel.setBorder(new EmptyBorder(2, 10, 10, 10));
 		alert_label.setBounds(10, 10, 20, 20);
-		alert_label.setFont(LFrame.font_ui_hallyos.deriveFont(16.0F));
+		alert_label.setFont(font.deriveFont(16.0F));
 		alert_label.setForeground(Color.WHITE);
 		alert_panel.add(alert_label);
 		return alert_panel;
